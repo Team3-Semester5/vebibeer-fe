38c67ffe2d2a7f60eeb41ebe1d19b9654d063fa6
NhatTV <79240278+truongnhat2102@users.noreply.github.com>
2024-05-18 13:38:47 +0700

    update nhat 18.05.2024

---
+++ src/main/java/com/example/vebibeer_be/VebibeerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.vebibeer_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VebibeerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VebibeerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vebibeer_be/VebibeerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vebibeer_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebibeerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
